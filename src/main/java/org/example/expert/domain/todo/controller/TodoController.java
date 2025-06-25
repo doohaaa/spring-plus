7dfc797727ffc31db280cafa0a9d5d27cb63da7b
doohaaa <engk5423@naver.com>
2025-06-25 21:56:17 +0900

    lv1-3 코드 개선 퀴즈 (JPA의 이해)

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -11,6 +11,7 @@ import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,12 +27,14 @@ public class TodoController {
         return ResponseEntity.ok(todoService.saveTodo(authUser, todoSaveRequest));
     }
 
-    @GetMapping("/todos")
+    @GetMapping("/todos?weather={weather}&startAt={startAt}&endAt={endAt}")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String weather,
+            @RequestParam(required = false) LocalDateTime startAt,
+            @RequestParam(required = false)LocalDateTime endAt) {
+        return ResponseEntity.ok(todoService.getTodos(page, size,weather, startAt, endAt));
     }
 
     @GetMapping("/todos/{todoId}")
